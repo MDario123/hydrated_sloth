988cbd7beb59267c131c6501d826823e6e7814d2
MDario123 <manuel.dario.oliver@gmail.com>
2024-09-22 00:53:04 +0200

    feat: better help

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -15,5 +15,12 @@ pub enum Subcomm {
     #[command(about = "Update water information.")]
     Water,
     #[command(about = "Update sleep information.")]
-    Sleep { from: DateTime<Local> },
+    Sleep {
+        #[arg(
+            long_help = "Went to sleep at this point in time. Assumed to wake up right now.
+Must be in format RFC 3339. Recommended to generate with `date`.
+e.g. `date -d '8 hours ago' --rfc-3339 c`"
+        )]
+        from: DateTime<Local>,
+    },
 }
